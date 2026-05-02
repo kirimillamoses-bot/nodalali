084fc259862d12087b3fda1586b38a9ce01ba67b
moses <kirimillamoses@gmail.com>
2026-05-02 14:33:18 +0300

    Replace SVG hero with realistic Serengeti sunset photo

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'nodalali-v2';
-const ASSETS = ['/', '/index.html', '/style.css', '/script.js', '/manifest.json', '/icon.svg', '/hero.svg'];
+const CACHE = 'nodalali-v3';
+const ASSETS = ['/', '/index.html', '/style.css', '/script.js', '/manifest.json', '/icon.svg', '/hero.jpg'];
 
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
